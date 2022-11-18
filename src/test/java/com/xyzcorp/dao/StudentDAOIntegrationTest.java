cb7dd268bfe83944aac850d680547cb2558bdb68
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-11-18 08:35:54 -0700

    Created the source

--- src/test/java/com/xyzcorp/dao/StudentDAOIntegrationTest.java
+++ src/test/java/com/xyzcorp/dao/StudentDAOIntegrationTest.java
@@ -1,8 +1,12 @@
 package com.xyzcorp.dao;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.postgresql.ds.PGConnectionPoolDataSource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.sql.SQLException;
 
 @Testcontainers
 public class StudentDAOIntegrationTest {
@@ -11,6 +15,18 @@ public class StudentDAOIntegrationTest {
     private static final PostgreSQLContainer<?> postgreSQLContainer =
         new PostgreSQLContainer<>("postgres:14.5")
             .withDatabaseName("school");
+    private static PGConnectionPoolDataSource source;
+
+    @BeforeAll
+    static void setUp() {
+        source = new PGConnectionPoolDataSource();
+        source.setURL(postgreSQLContainer.getJdbcUrl());
+        source.setUser(postgreSQLContainer.getUsername());
+        source.setPassword(postgreSQLContainer.getPassword());
+        source.setDatabaseName("school");
+    }
+
+
 
 
 }
